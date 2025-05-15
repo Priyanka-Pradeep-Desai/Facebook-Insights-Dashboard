18e3f94bd6791f1f7ebfa25bff698f535b544810
Priyanka Pradeep Desai <priyankapradeepdesai@gmail.com>
2025-05-15 13:29:53 -0500

    Update run_dashboard.py

--- run_dashboard.py
+++ run_dashboard.py
@@ -518,7 +518,7 @@ if should_send_email_gsheet():
     try:
         sender_email = st.secrets["GMAIL_USER"]
         password = st.secrets["GMAIL_PASS"]
-        receiver_emails = ["priyankadesai1999@gmail.com", "priyankapradeepdesai@gmail.com"]
+        receiver_emails = ["priyankadesai1999@gmail.com", "tom.basey@gmail.com"]
 
         message = MIMEMultipart("alternative")
         message["Subject"] = "📊 Facebook Dashboard Link"
